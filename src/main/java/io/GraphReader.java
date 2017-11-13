52c701d585be858637e8160c978134f5b3986d41
Mark <mark.ortmann@uni-konstanz.de>
2017-11-13 15:18:04 +0100

    fixed exception handling

--- src/main/java/io/GraphReader.java
+++ src/main/java/io/GraphReader.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of sparse-stress.
- *
- * Copyright (C) 2016-2017 Mark Ortmann (University of Konstanz)
- *
- * sparse-stress is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sparse-stress is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with sparse-stress.  If not, see <http://www.gnu.org/licenses/>.
- */
 package io;
 
 import java.io.BufferedReader;
@@ -34,7 +16,6 @@ public final class GraphReader {
 
 	@SuppressWarnings("unchecked")
 	public static Graph readGraph(final Options options) throws IllegalArgumentException {
-
 		final File file = options.getFile();
 		final boolean weighted = options.isWeighted();
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -44,13 +25,18 @@ public final class GraphReader {
 				for (int i = 0; i < eList.length; i++) {
 					eList[i] = new ArrayList<Pair>();
 				}
-			} catch (IOException e) {
-				throw new IllegalArgumentException("first line has to contain the number of nodes");
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("first line does not contain the number of nodes");
 			}
 			String line;
+			int lineNo = 0;
 			try {
 				while ((line = reader.readLine()) != null) {
+					++lineNo;
 					String[] line_split = line.split(DELIMITER);
+					if(line_split.length < 2){
+						throw new IllegalArgumentException("line " + lineNo + " does not obey the required format");
+					}
 					double weight = DEFAULT_LENGTH;
 					if (weighted) {
 						try {
