9079e83275d142a48555821f011ea9b7c402621f
Mark <mark.ortmann@uni-konstanz.de>
2017-11-13 15:28:13 +0100

    forgot license

--- src/main/java/io/GraphReader.java
+++ src/main/java/io/GraphReader.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of sparse-stress.
+ *
+ * Copyright (C) 2016-2017 Mark Ortmann (University of Konstanz)
+ *
+ * sparse-stress is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sparse-stress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sparse-stress.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io;
 
 import java.io.BufferedReader;
@@ -16,6 +34,7 @@ public final class GraphReader {
 
 	@SuppressWarnings("unchecked")
 	public static Graph readGraph(final Options options) throws IllegalArgumentException {
+
 		final File file = options.getFile();
 		final boolean weighted = options.isWeighted();
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
